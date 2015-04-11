ce35cd33a1a295e09c0252f35b63f55dda7276fd
Randy Patterson <randy@randypatterson.com>
2015-04-11 03:47:04 -0400

    added GET

--- MyIoT/Controllers/SensorDataController.cs
+++ MyIoT/Controllers/SensorDataController.cs
@@ -1,33 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
 
 namespace MyIoT.Controllers
 {
     public class SensorDataController : Controller
     {
-        [HttpPost]
         public void Index()
         {
-            string data = string.Empty;
+            var queryString = HttpContext.Request.RawUrl;
+            queryString = queryString.Substring(queryString.IndexOf('?') + 1);
+            pushMessage(queryString);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public void Index_post()
+        {
+            var data = string.Empty;
             //Convert payload to a string
-            using (StreamReader reader = new StreamReader(Request.InputStream, Encoding.UTF8))
+            using (var reader = new StreamReader(Request.InputStream, Encoding.UTF8))
                 data = reader.ReadToEnd();
 
-            string message = string.Format("{0}: {1}", DateTime.Now.ToLongTimeString(), data);
+            pushMessage(data);
+        }
 
+        private static void pushMessage(string message)
+        {
+            message = string.Format("{0}: {1}", DateTime.Now.ToLongTimeString(), message);
             //push incoming data out to any connected browsers
-            IHubConnectionContext<dynamic> clients = GlobalHost.ConnectionManager.GetHubContext<SensorDataHub>().Clients;
+            var clients = GlobalHost.ConnectionManager.GetHubContext<SensorDataHub>().Clients;
             clients.All.addNewMessageToPage(message);
-
-
-
         }
     }
 }
